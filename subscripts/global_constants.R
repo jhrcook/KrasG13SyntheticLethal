46c07f4f453e9ac26176793a2aee389816d38fb6
Josh Cook <joshuacook0023@gmail.com>
2019-06-12 15:09:33 -0400

    initial commit

---
+++ subscripts/global_constants.R
@@ -0,0 +1,12 @@
+
+ras_pal <- c(
+    KRAS = "tomato",
+    NRAS = "dodgerblue",
+    HRAS = "mediumseagreen"
+)
+
+organs_pal <- c(
+    colorectal = "chocolate1",
+    lung = "darkorchid2",
+    pancreas = "olivedrab3"
+)
