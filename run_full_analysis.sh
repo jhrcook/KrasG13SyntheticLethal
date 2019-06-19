fccbf27906b115a731b62afdf6d67eae52971c2c
Josh Cook <joshuacook0023@gmail.com>
2019-06-19 19:39:29 -0400

    style: rebuilt all images - little information change

--- run_full_analysis.sh
+++ run_full_analysis.sh
@@ -8,5 +8,15 @@ Rscript subscripts/data_preparation.R
 # linear model
 Rscript subscripts/linear_model.R
 
+# predicting KRAS mutation by genetic dependencies
+Rscript subscripts/predict_rasallele.R
+
+# check overlap with Alex (UCSF)
+Rscript subscripts/overlap_alex.R
+
+# functional annotation of genetic dependency hits
+Rscript subscripts/hit_annotation.R
+
 # render README
 Rscript -e 'rmarkdown::render("README.Rmd")'
+
