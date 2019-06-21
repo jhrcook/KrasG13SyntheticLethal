9b579e92cce4a39e58706ab16be81babac9cb665
Josh Cook <joshuacook0023@gmail.com>
2019-06-21 13:10:20 -0400

    run hit_annotation.R before overlap_alex.R

--- run_full_analysis.sh
+++ run_full_analysis.sh
@@ -11,11 +11,11 @@ Rscript subscripts/linear_model.R
 # predicting KRAS mutation by genetic dependencies
 Rscript subscripts/predict_rasallele.R
 
-# check overlap with Alex (UCSF)
-Rscript subscripts/overlap_alex.R
-
 # functional annotation of genetic dependency hits
 Rscript subscripts/hit_annotation.R
+
+# check overlap with Alex (UCSF)
+Rscript subscripts/overlap_alex.R
 
 # render README
 Rscript -e 'rmarkdown::render("README.Rmd")'
