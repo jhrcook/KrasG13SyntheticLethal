625058f273dd8d257361ccb35dd6c62e2401955a
Josh Cook <joshuacook0023@gmail.com>
2019-06-20 20:56:53 -0400

    refactor: automatically add named colors in palette without underscores

--- subscripts/global_constants.R
+++ subscripts/global_constants.R
@@ -15,8 +15,8 @@ allele_pal <- c(
     KRAS_G13D = "tomato",
     KRAS_G12 = "navy",
     KRAS_G12D = "mediumorchid4",
-    "KRAS G13D" = "tomato",
-    "KRAS G12" = "navy",
-    "KRAS G12D" = "mediumorchid4",
     WT = "grey50"
 )
+allele_pal_no_underscore_names <- allele_pal
+names(allele_pal_no_underscore_names) <- stringr::str_replace_all(names(allele_pal), "_", " ")
+allele_pal <- c(allele_pal, allele_pal_no_underscore_names)
