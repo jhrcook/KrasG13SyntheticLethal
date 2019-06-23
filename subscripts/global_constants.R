f49aa0b444184bb810bd9f2c7c30090093b61587
Josh Cook <joshuacook0023@gmail.com>
2019-06-22 21:14:46 -0400

    style: change WT color to darker grey

--- subscripts/global_constants.R
+++ subscripts/global_constants.R
@@ -15,7 +15,7 @@ allele_pal <- c(
     KRAS_G13D = "tomato",
     KRAS_G12 = "navy",
     KRAS_G12D = "mediumorchid4",
-    WT = "grey50"
+    WT = "grey30"
 )
 allele_pal_no_underscore_names <- allele_pal
 names(allele_pal_no_underscore_names) <- stringr::str_replace_all(names(allele_pal), "_", " ")
