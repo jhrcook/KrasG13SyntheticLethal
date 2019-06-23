3c5e559d11ce3eae6d701c65463cec930c6cbb4b
Josh Cook <joshuacook0023@gmail.com>
2019-06-23 18:58:05 -0400

    add sample_CI.R and final_list.R to run

--- run_full_analysis.sh
+++ run_full_analysis.sh
@@ -11,12 +11,17 @@ Rscript subscripts/linear_model.R
 # predicting KRAS mutation by genetic dependencies
 Rscript subscripts/predict_rasallele.R
 
-# functional annotation of genetic dependency hits
-Rscript subscripts/hit_annotation.R
-
 # check overlap with Alex (UCSF)
 Rscript subscripts/overlap_alex.R
 
+# functional annotation of genetic dependency hits
+Rscript subscripts/hit_annotation.R
+
+# bootstrap 95% CI for model coefficients
+Rscript subscripts/sample_CI.R
+
+# save result data to Excel file
+Rscript subscripts/final_lists.R
+
 # render README
 Rscript -e 'rmarkdown::render("README.Rmd")'
-
