dae9f96efc3a2a6220140842ecd8f9eb68cf54dd
Josh Cook <joshuacook0023@gmail.com>
2019-06-24 08:35:50 -0400

    feat: add cell line names (not just DepMap ID)

--- subscripts/final_lists.R
+++ subscripts/final_lists.R
@@ -2,7 +2,6 @@
 ## Write out the final lists for further use ##
 ###############################################
 
-
 library(tidyverse)
 
 source(file.path("subscripts", "global_constants.R"))
@@ -16,9 +15,8 @@ output_excel_file <- "KRAS_G13D_genetic_dependency.xlsx"
 #### ---- Prepare Input Data ---- ####
 
 dep_map <- readRDS(file.path("data", "Achilles_gene_effect.tib"))
-ccle_muts <- readRDS(file.path("data", "cell_line_mutations.tib"))
-ras_muts <- readRDS(file.path("data", "ras_mutants_info.tib"))
 model_data <- readRDS(file.path("model_results", "model_data.rds"))
+
 
 #### ---- Prepare model results ---- ####
 
@@ -38,8 +36,24 @@ final_list_of_hits <- c(
     "ZBTB17"
 )
 
+# get information on the cell lines
+cell_line_tib <- dep_map %>%
+    select(dep_map_id, stripped_cell_line_name, ccle_name) %>%
+    filter(dep_map_id %in% unique(model_data$dep_map_id)) %>%
+    dplyr::rename(cell_line_name = "stripped_cell_line_name") %>%
+    unique() %>%
+    arrange(dep_map_id)
 
-cell_line_tib <- model_data %>%
+# add cell line info to `model_data` and reorganize columns
+model_data <- left_join(model_data, cell_line_tib, by = "dep_map_id") %>%
+    select(
+        dep_map_id, cell_line_name, ccle_name, disease,
+        ras_allele, codon,
+        gene, gene_effect, target_is_mutated
+    )
+
+# spread by if the target is mutated or not
+model_data %>%
     select(-gene_effect) %>%
     filter(gene %in% !!final_list_of_hits) %>%
     mutate(target_is_mutated = ifelse(target_is_mutated, "T", ".")) %>%
@@ -48,6 +62,7 @@ cell_line_tib <- model_data %>%
                      sheetName = "cell lines",
                      append = FALSE)
 
+# spread by the gene effect
 model_data %>%
     select(-target_is_mutated) %>%
     filter(gene %in% !!final_list_of_hits) %>%
@@ -57,8 +72,15 @@ model_data %>%
                      sheetName = "dependency scores",
                      append = TRUE)
 
+# all data in tall (tidy) format
 model_data %>%
     filter(gene %in% !!final_list_of_hits) %>%
     xlsx::write.xlsx(file = output_excel_file,
                      sheetName = "all data",
                      append = TRUE)
+
+# just cell line info
+xlsx::write.xlsx(cell_line_tib,
+                 file = output_excel_file,
+                 sheetName = "cell line names",
+                 append = TRUE)
